657069ffe4ce891769f49df5cf514cff57f91aac
vsk4 <saikumarvadla4@gmail.com>
2020-10-21 18:40:38 +0530

    [VSK]: Added UC1_ LinkedList

---
+++ DataStructures/DataStructures/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS_Demo
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+
+
+
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DS_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 450);
+
+            list.Display();
+
+        }
+    }
+}
