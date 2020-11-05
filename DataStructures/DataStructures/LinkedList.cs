1a36c41be2e935f532868cdd24d6154f8ffb5558
SAI KUMAR <saikumarvadla4@gmail.com>
2020-11-05 19:16:50 +0530

    [VSK]: Refractor UC1, made changes

--- DataStructures/DataStructures/LinkedList.cs
+++ DataStructures/DataStructures/LinkedList.cs
@@ -1,13 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace DS_Demo
+namespace LinkedListProject
 {
-    class LinkedList
+    public class LinkedList
     {
         internal Node head;
-        //internal Node Tail;
         internal void Add(int data)
         {
             Node node = new Node(data);
@@ -38,72 +37,5 @@ namespace DS_Demo
                 temp = temp.next;
             }
         }
-        internal Node InsertAtParticularPosition(int position, int data)
-        {
-            if (position < 1)
-                Console.WriteLine("Invalid position");
-            if (position == 1)
-            {
-                var newNode = new Node(data);
-                newNode.next = this.head;
-                head = newNode;
-            }
-            else
-            {
-                while (position-- != 0)
-                {
-                    if (position == 1)
-                    {
-                        Node node = new Node(data);
-                        node.next = this.head.next;
-                        head.next = node;
-                        break;
-                    }
-                    head = head.next;
-                }
-                if (position != 1)
-                    Console.WriteLine("Position out of range");
-            }
-            return head;
-        }
-
-        internal Node RemoveFirstNode()
-        {
-            if (this.head == null)
-                return null;
-            this.head = this.head.next;
-            return this.head;
-        }
-
-        internal Node RemovaLastNode()
-        {
-            if (head == null)
-                return null;
-            if (head.next == null)
-                return null;
-            Node newNode = head;
-            while (newNode.next.next != null)
-            {
-                newNode = newNode.next;
-            }
-            newNode.next = null;
-            return head;
-        }
-
-        internal Node Search(int value)
-        {
-            while (this.head != null)
-            {
-                if (this.head.data == value)
-                {
-                    return this.head;
-                }
-                this.head = this.head.next;
-            }
-            return null;
-        }
-
-
-
     }
 }
